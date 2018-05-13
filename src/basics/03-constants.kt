b55bca89689c3bed6111855d12e9cedb687f412f
jakublabenski <25360840+jakublabenski@users.noreply.github.com>
2018-05-13 21:41:43 +0200

    Example with constant declaration without type

--- src/basics/03-constants.kt
+++ src/basics/03-constants.kt
@@ -3,15 +3,17 @@ package basics
 fun test(arg: Int): Int {
     // constant initialized instantly:
     val str1: String = "abc"
+    // with skipped type:
+    val str2 = "abc"
 
     // constant with delayed initialization
-    val str2: String
+    val str3: String
     if (arg > 0) {
-        str2 = "x1"
+        str3 = "x1"
     } else {
-        str2 = "x2"
+        str3 = "x2"
     }
-    if (str2.length > 5) {
+    if (str3.length > 5) {
 
     }
 
